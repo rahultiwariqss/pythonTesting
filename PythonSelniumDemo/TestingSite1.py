858210813f439f0f79a35407885a9e249676305a
rahultiwariqss <rahul.tiwari@qsstechnosoft.com>
2023-03-02 01:01:49 +0530

    SeleniumFirstSite

--- PythonSelniumDemo/TestingSite1.py
+++ PythonSelniumDemo/TestingSite1.py
@@ -1,4 +1,7 @@
+import time
+
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
@@ -17,54 +20,72 @@ class TestingSite1:
     print(driver.title)
     driver.implicitly_wait(4)
 
-    #Radio button selection using specific name and check if radion button is enabled/selected
-    allRadioButtons = driver.find_elements(By.XPATH, "//input[@type='radio']")
-    print(len(allRadioButtons))
+#     #Radio button selection using specific name and check if radion button is enabled/selected
+#     allRadioButtons = driver.find_elements(By.XPATH, "//input[@type='radio']")
+#     print(len(allRadioButtons))
+#
+#     for radio in allRadioButtons:
+#         if radio.get_attribute("value") == "radio2":
+#             radio.click()
+#             if radio.is_selected():
+#                 print("Radio is select")
+#             else:
+#                 print("Radio button is not selected")
+#             break
+#
+# # Suggestion Class Example(Dynamic dropdown example
+#
+#     driver.find_element(By.XPATH, "//input[@id='autocomplete']").send_keys("Ind")
+#     allCountries = driver.find_elements(By.XPATH, "//ul[@id='ui-id-1']/li")
+#     print(len(allCountries))
+#
+#
+#     for country in allCountries:
+#
+#         if country.text == "India":
+#             country.click()
+#             break
+#
+#
+#
+# # Select drop down
+#     dropdown = Select (driver.find_element(By.XPATH,"//select[@id='dropdown-class-example']"))
+#     dropdown.select_by_index(1)
+#
+# # Switch Window Example
+#     driver.find_element(By.XPATH, "//button[@id='openwindow']").click()
+#     print(driver.title)
+#     windows= driver.window_handles
+#     driver.switch_to.window(windows[1])
+#     print(driver.title)
+#     driver.switch_to.window(windows[0])
+#     print(driver.title)
 
-    for radio in allRadioButtons:
-        if radio.get_attribute("value") == "radio2":
-            radio.click()
-            if radio.is_selected():
-                print("Radio is select")
-            else:
-                print("Radio button is not selected")
-            break
 
-# Suggestion Class Example(Dynamic dropdown example
+# # Switch Window Example
+#     driver.find_element(By.XPATH, "//a[contains(text(),'Open Tab')]").click()
+#     print(driver.title)
+#     win = driver.window_handles
+#     driver.switch_to.window(win[1])
+#     print(driver.title)
 
-    driver.find_element(By.XPATH, "//input[@id='autocomplete']").send_keys("Ind")
-    allCountries = driver.find_elements(By.XPATH, "//ul[@id='ui-id-1']/li")
-    print(len(allCountries))
+# #Alert Handle
+#     driver.find_element(By.XPATH, "//input[@id='name']").send_keys("Rahul")
+#     driver.find_element(By.XPATH, "//input[@id='alertbtn']").click()
+#     print(driver.switch_to.alert.text)
+#     driver.switch_to.alert.accept()
 
+#Scroll page
+    driver.execute_script("window.scrollBy(0,1200)")
+    time.sleep(2)
+# ActionsChain
 
-    for country in allCountries:
+    action = ActionChains(driver)
+    action.move_to_element(driver.find_element(By.XPATH,"//button[@id='mousehover']")).perform()
 
-        if country.text == "India":
-            country.click()
-            break
-
-
-
-# Select drop down
-    dropdown = Select (driver.find_element(By.XPATH,"//select[@id='dropdown-class-example']"))
-    dropdown.select_by_index(1)
-
-# Switch Window Example
-    driver.find_element(By.XPATH, "//button[@id='openwindow']").click()
-    print(driver.title)
-    windows= driver.window_handles
-    driver.switch_to.window(windows[1])
-    print(driver.title)
-    driver.switch_to.window(windows[0])
-    print(driver.title)
-
-
-# Switch Window Example
-    driver.find_element(By.XPATH, "//a[contains(text(),'Open Tab')]").click()
-    print(driver.title)
-    win = driver.window_handles
-    driver.switch_to.window(win[1])
-    print(driver.title)
+    driver.execute_script("window.scrollBy(0,100)")
+    driver.switch_to.frame("courses-iframe")
+    driver.find_element(By.LINK_TEXT, "Blog").click()
 
 
 
