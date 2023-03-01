0f8380dde91dbb2581ff9d3351c8f0d170b44324
rahultiwariqss <rahul.tiwari@qsstechnosoft.com>
2023-03-01 23:35:31 +0530

    SeleniumFirstSite

--- PythonSelniumDemo/TestingSite1.py
+++ PythonSelniumDemo/TestingSite1.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 
 
 class TestingSite1:
@@ -42,10 +43,29 @@ class TestingSite1:
             country.click()
             break
 
-    result= []
-    for country in allCountries:
-        result.append()
 
-    print(result)
+
+# Select drop down
+    dropdown = Select (driver.find_element(By.XPATH,"//select[@id='dropdown-class-example']"))
+    dropdown.select_by_index(1)
+
+# Switch Window Example
+    driver.find_element(By.XPATH, "//button[@id='openwindow']").click()
+    print(driver.title)
+    windows= driver.window_handles
+    driver.switch_to.window(windows[1])
+    print(driver.title)
+    driver.switch_to.window(windows[0])
+    print(driver.title)
+
+
+# Switch Window Example
+    driver.find_element(By.XPATH, "//a[contains(text(),'Open Tab')]").click()
+    print(driver.title)
+    win = driver.window_handles
+    driver.switch_to.window(win[1])
+    print(driver.title)
+
+
 
 
